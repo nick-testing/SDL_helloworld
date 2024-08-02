38b568505da36cd4a1924bd196a2a798ff7162df
Nick <nick@example.com>
2024-08-02 22:25:12 +0300

    Added Init function

--- src/HelloWorld.cpp
+++ src/HelloWorld.cpp
@@ -7,13 +7,38 @@ HelloWorld::HelloWorld():
     screenSurface(nullptr), 
     helloWorldSurface(nullptr) {}
 
-
 bool HelloWorld::Init() {
+    bool success = true;
 
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        std::cerr << "SDL: Initialization failed. Error: " << SDL_GetError() << std::endl;
+        success = false;
+    } 
+    else {
+        // Create window
+        window = SDL_CreateWindow("Hello World!", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+                                  SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+        
+        if (!window) {
+            std::cerr << "SDL: window creation failed. Error: " << SDL_GetError() << std::endl;
+            SDL_Quit();
+            success = false;
+        }
+        else {
+            // Get window surface
+            screenSurface = SDL_GetWindowSurface(window);
+        }
+    }
+
+    return success;
 }
 
 bool HelloWorld::LoadMedia() {
+    bool success = true;
 
+    helloWorldSurface = SDL_LoadBMP("../assets/helloworld.bmp");
+
+    return success;
 }
 
 void HelloWorld::Close() {
