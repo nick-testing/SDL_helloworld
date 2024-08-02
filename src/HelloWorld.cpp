3817afa683112704b2179e7e2a9a799714904a94
Nick <nick@example.com>
2024-08-02 21:39:51 +0300

    added function definitions

--- src/HelloWorld.cpp
+++ src/HelloWorld.cpp
@@ -1,1 +1,24 @@
 #include "HelloWorld.h"
+#include <SDL2/SDL.h>
+#include <iostream>
+
+static const int SCREEN_HEIGHT = 720;
+static const int SCREEN_WIDTH = 1280;
+
+HelloWorld::HelloWorld(): 
+    gWindow(nullptr), 
+    gScreenSurface(nullptr), 
+    gHelloWorld(nullptr) {}
+
+
+bool HelloWorld::Init() {
+
+}
+
+bool HelloWorld::LoadMedia() {
+
+}
+
+void HelloWorld::Close() {
+
+}
